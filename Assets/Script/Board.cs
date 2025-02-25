b213419b9cef49bd34b5d31fe0802b2890a4b298
yubinx <yubinxie222@gmail.com>
2025-02-25 13:10:08 -0500

    Piece detected when mouse clicking on it

--- Assets/Script/Board.cs
+++ Assets/Script/Board.cs
@@ -38,46 +38,65 @@ public class ChessBoard : MonoBehaviour
     public GameObject blackQueenPrefab;
     public GameObject blackKingPrefab;
     
-
+    //Instantiate All Pieces on Board
     void Start()
     {
         InstantiateBoard();
         InstantiatePieces();
     }
-
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Mouse Clicked");
+            HandleClick();
+        }
+    }
     void InstantiateBoard()
     {
         Instantiate(boardPrefab, boardOrigin, Quaternion.identity);
     }
     void InstantiatePieces()
     {
-    for (int i = 0; i < 64; i++)
-    {
-        if ((WhitePawn & (1UL << i)) != 0)
-        Instantiate(whitePawnPrefab, GetWorldPositionForBit(i), Quaternion.identity);
-        if ((WhiteRook & (1UL << i)) != 0)
-        Instantiate(whiteRookPrefab, GetWorldPositionForBit(i), Quaternion.identity);
-        if ((WhiteKnight & (1UL << i)) != 0)
-        Instantiate(whiteKnightPrefab, GetWorldPositionForBit(i), Quaternion.identity);
-        if ((WhiteBishop & (1UL << i)) != 0)
-        Instantiate(whiteBishopPrefab, GetWorldPositionForBit(i), Quaternion.identity);
-        if ((WhiteQueen & (1UL << i)) != 0)
-        Instantiate(whiteQueenPrefab, GetWorldPositionForBit(i), Quaternion.identity);
-        if ((WhiteKing & (1UL << i)) != 0)
-        Instantiate(whiteKingPrefab, GetWorldPositionForBit(i), Quaternion.identity);
-        if ((BlackPawn & (1UL << i)) != 0)
-        Instantiate(blackPawnPrefab, GetWorldPositionForBit(i), Quaternion.identity);
-        if ((BlackRook & (1UL << i)) != 0)
-        Instantiate(blackRookPrefab, GetWorldPositionForBit(i), Quaternion.identity);
-        if ((BlackKnight & (1UL << i)) != 0)
-        Instantiate(blackKnightPrefab, GetWorldPositionForBit(i), Quaternion.identity);
-        if ((BlackBishop & (1UL << i)) != 0)
-        Instantiate(blackBishopPrefab, GetWorldPositionForBit(i), Quaternion.identity);
-        if ((BlackQueen & (1UL << i)) != 0)
-        Instantiate(blackQueenPrefab, GetWorldPositionForBit(i), Quaternion.identity);
-        if ((BlackKing & (1UL << i)) != 0)
-        Instantiate(blackKingPrefab, GetWorldPositionForBit(i), Quaternion.identity);
+        for (int i = 0; i < 64; i++)
+        {
+            if ((WhitePawn & (1UL << i)) != 0)
+            Instantiate(whitePawnPrefab, GetWorldPositionForBit(i), Quaternion.identity);
+            if ((WhiteRook & (1UL << i)) != 0)
+            Instantiate(whiteRookPrefab, GetWorldPositionForBit(i), Quaternion.identity);
+            if ((WhiteKnight & (1UL << i)) != 0)
+            Instantiate(whiteKnightPrefab, GetWorldPositionForBit(i), Quaternion.identity);
+            if ((WhiteBishop & (1UL << i)) != 0)
+            Instantiate(whiteBishopPrefab, GetWorldPositionForBit(i), Quaternion.identity);
+            if ((WhiteQueen & (1UL << i)) != 0)
+            Instantiate(whiteQueenPrefab, GetWorldPositionForBit(i), Quaternion.identity);
+            if ((WhiteKing & (1UL << i)) != 0)
+            Instantiate(whiteKingPrefab, GetWorldPositionForBit(i), Quaternion.identity);
+            if ((BlackPawn & (1UL << i)) != 0)
+            Instantiate(blackPawnPrefab, GetWorldPositionForBit(i), Quaternion.identity);
+            if ((BlackRook & (1UL << i)) != 0)
+            Instantiate(blackRookPrefab, GetWorldPositionForBit(i), Quaternion.identity);
+            if ((BlackKnight & (1UL << i)) != 0)
+            Instantiate(blackKnightPrefab, GetWorldPositionForBit(i), Quaternion.identity);
+            if ((BlackBishop & (1UL << i)) != 0)
+            Instantiate(blackBishopPrefab, GetWorldPositionForBit(i), Quaternion.identity);
+            if ((BlackQueen & (1UL << i)) != 0)
+            Instantiate(blackQueenPrefab, GetWorldPositionForBit(i), Quaternion.identity);
+            if ((BlackKing & (1UL << i)) != 0)
+            Instantiate(blackKingPrefab, GetWorldPositionForBit(i), Quaternion.identity);
+        }
     }
+    void HandleClick()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            if(hit.collider.tag == "ChessPiece")
+            {
+                Debug.Log("Chess Piece Clicked");
+            }
+        }
     }
     Vector3 GetWorldPositionForBit(int bitIndex)
     {
